df4d8d27fb832f5550d18bfe1bbcbb32fd1a2f73
Aziza <aziza.shestakova@nutnet.ru>
2018-09-02 21:49:40 +0300

    слайдер

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,7 +1,76 @@
-'use strict';
+// 'use strict';
 
-import Icon from '../_modules/icon/icon';
+var fiveDaysContainer = document.getElementById('fiveDaysContainer'); 
+var elements = document.querySelectorAll('.temperature-daytime');
+var tabs = document.querySelectorAll('.tab'); 
+// var nextTable = document.getElementsByClassName('temperature-daytime'); 
 
-(() => {
-  new Icon();
-})();
+fiveDaysContainer.onclick = function(event) { 
+  if (event.target.className === 'next') { 
+    showNext() 
+  }
+  if (event.target.className === 'prev') { 
+    showPrev() 
+  } 
+  if (event.target.className === 'tab') { 
+    showDay()
+  }
+} 
+
+function showNext() { 
+  for (var i = 0; i < elements.length; i++) { 
+    if (elements[i].classList.contains('active')) { 
+      elements[i].classList.remove('active') 
+
+      if (i === elements.length - 1) { 
+        elements[0].classList.add('active') 
+        break 
+      } 
+
+      elements[i + 1].classList.add('active') 
+      break 
+    } 
+  } 
+}
+
+function showPrev() { 
+  for (var i = 0; i < elements.length; i++) { 
+    if (elements[i].classList.contains('active')) { 
+      elements[i].classList.remove('active') 
+
+      if (i === 0) { 
+        elements[elements.length - 1].classList.add('active') 
+        break 
+      } 
+
+      elements[i - 1].classList.add('active') 
+      break 
+    } 
+  } 
+}
+
+function showDay() { 
+  for (var i = 0; i < elements.length; i++) { 
+    if (elements[i].classList.contains('active')) { 
+      elements[i].classList.remove('active') 
+
+      // if (i === elements.length - 1) { 
+      //   elements[0].classList.add('active') 
+      //   break 
+      // } 
+
+      // if (i === elements.length - 1) { 
+      //   elements[0].classList.add('active') 
+      //   break 
+      // } 
+
+      // if(i === elements[i]) {
+      //   elements[3].classList.add('active')
+      //   break
+      // }
+
+      elements[i + 1].classList.add('active') 
+      break 
+    } 
+  } 
+}
